088504bc26ae6bd053ff8df9cb3341b22f20d8c9
Xentia Damascus <JacobVanthof0504@gmail.com>
2025-07-03 11:48:51 -0700

    Add menufacture and setup toggle term

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,10 +6,12 @@ return {
         dependencies = {
             'nvim-lua/plenary.nvim', 
             -- Add fuzzy finder plugin
-            {'nvim-telescope/telescope-fzf-native.nvim', 
+            {
+                'nvim-telescope/telescope-fzf-native.nvim', 
                 build  = 'make', 
             },
-            {'nvim-telescope/telescope-ui-select.nvim'},
+            'nvim-telescope/telescope-ui-select.nvim',
+            'molecule-man/telescope-menufacture',
         },
         config  = function()
             require('telescope').setup {
@@ -28,15 +30,15 @@ return {
 
             -- Setup leader binds
             local builtin = require 'telescope.builtin'
-            vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-            vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-            vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+            vim.keymap.set('n', '<leader>sf', require('telescope').extensions.menufacture.find_files, { desc = '[S]earch [F]iles' })
+            vim.keymap.set('n', '<leader>sw', require('telescope').extensions.menufacture.grep_string, { desc = '[S]earch current [W]ord' })
+            vim.keymap.set('n', '<leader>sg', require('telescope').extensions.menufacture.live_grep, { desc = '[S]earch by [G]rep' })
+            vim.keymap.set('n', '<leader>s.', require('telescope').extensions.menufacture.live_grep, { desc = '[S]earch Recent Files ("." for repeat)' })
             vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-            vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-            vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
             vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
             vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-            vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+            vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+            vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
             vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
             -- LSP builtin bindings
---
+++ lua/plugins/terminal.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        'akinsho/toggleterm.nvim',
+        version = '*',
+        opts = {
+            open_mapping = [[<c-t>]],
+            direction = 'float',
+            shade_terminals = 'true',
+            shading_factor = '-100',
+            shading_ratio = '-50',
+            winblend = '10',
+        }
+    }
+}
