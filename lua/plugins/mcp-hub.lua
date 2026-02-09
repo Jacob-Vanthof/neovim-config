27d6589e62548f1dc88e04de60d0cf28d061662d
Xentia Damascus <JacobVanthof0504@gmail.com>
2026-02-09 14:14:51 -0800

    Add copilot chat functionality for mcphub plugin

--- lua/plugins/mcp-hub.lua
+++ lua/plugins/mcp-hub.lua
@@ -6,7 +6,17 @@ return {
         },
         build = "npm install -g mcp-hub@latest",  -- Installs `mcp-hub` node binary globally
         config = function()
-            require("mcphub").setup()
+            require("mcphub").setup({
+                extensions = {
+                    -- Add the copilot extension
+                    copilotchat = {
+                        enabled = true,
+                        covert_tools_to_functions = true,
+                        convert_resources_to_functions = true,
+                        add_mcp_prefex = false,
+                    },
+                },
+            })
         end
     }
 }
